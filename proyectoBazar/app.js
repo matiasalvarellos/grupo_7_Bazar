6e4ac13e404d92406c64f990637915620afd2046
matiasalvarellos <matias.alvarellos@gmail.com>
2021-01-24 18:58:43 -0300

    refator frontend y fix middleware de locals

--- proyectoBazar/app.js
+++ proyectoBazar/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret:"Secretoo breoo!!",
-  resave: false ,
+  resave: true ,
   saveUninitialized: true 
 }));
 
