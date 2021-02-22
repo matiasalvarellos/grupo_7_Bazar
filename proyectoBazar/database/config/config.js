1df12ffd346e42d9510c524e81a41aedc6f391a5
karlita29 <lyversis@gmail.com>
2021-02-21 22:29:39 -0300

    cambios

--- proyectoBazar/database/config/config.js
+++ proyectoBazar/database/config/config.js
@@ -1,29 +1,29 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "",
+    "password": "vamos-2021",
     "database": "bazar_db",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306",
+    "port": "3101",
     "define":{
       "underscored": true
     }
   },
   "test": {
     "username": "root",
-    "password": '',
+    "password": "vamos-2021",
     "database": "bazar_db",
     "host": "127.0.0.1",
-    "port":"3306",
+    "port":"3101",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": '',
+    "password": "vamos-2021",
     "database": "bazar_db",
     "host": "127.0.0.1",
-    "port":"3306",
+    "port":"3101",
     "dialect": "mysql"
   }
 }
