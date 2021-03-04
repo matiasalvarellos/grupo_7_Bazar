da127398379bcfc4e60e7376f360c80658100fb5
matiasalvarellos <matias.alvarellos@gmail.com>
2021-03-04 16:13:20 -0300

    se agrego el carrito y los fetch

--- proyectoBazar/middlewares/adminMiddleware.js
+++ proyectoBazar/middlewares/adminMiddleware.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
    if (typeof req.session.usuarioLogueado !="undefined"){
         if(req.session.usuarioLogueado.admin == 1){
             return next()
-        } 
+        }
     }
     return res.redirect("/")
 }
